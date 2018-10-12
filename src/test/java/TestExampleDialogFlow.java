7e7561603b733d8f12e9295087edd6d7350e8cb2
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-10-12 11:35:42 +0200

    Init commit

---
+++ src/test/java/TestExampleDialogFlow.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/features",
+        glue= {"com.github.flock_se.dialogflow_ci.steps"},
+        tags = {"~@Ignore"},
+        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber.xml"},
+        monochrome = true,
+        dryRun = false
+)
+
+public class TestExampleDialogFlow extends com.github.flock_se.dialogflow_ci.DialogflowTestRunner {
+
+}
